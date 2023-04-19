8d2f7ac2d2f7160079d0f188718cad1833faa114
Daniel Barchuk <daniel.barchuk@yandex.ru>
2023-04-19 13:49:58 +0700

    norm

--- PR-15-BarchukDT-Bilet9-PM04/MainWindow.xaml.cs
+++ PR-15-BarchukDT-Bilet9-PM04/MainWindow.xaml.cs
@@ -24,5 +24,55 @@ namespace PR_15_BarchukDT_Bilet9_PM04
         {
             InitializeComponent();
         }
+
+        private void txt_login_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool isOk = LoginValidate(txt_login.Text);
+            if (isOk == false)
+            {
+                tb_login.Text = "Неверное количество символов";
+            }
+            else
+            {
+                tb_login.Text = "Количество символов в порядке";
+            }
+        }
+
+        private void txt_password_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            bool isOk = PasswordValidate(txt_password.Password);
+            if (isOk == false)
+            {
+                tb_password.Text = "Неверное количество символов";
+            }
+            else
+            {
+                tb_password.Text = "Количество символов в порядке";
+            }
+        }
+
+        public static bool PasswordValidate(string pass)
+        {
+            if (pass.Length < 10 || pass.Length > 100)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool LoginValidate(string log)
+        {
+            if (log.Length < 4 || log.Length > 35)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
     }
 }
